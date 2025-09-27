8da931f11ba241e34750ed07b30fc236ae424c2c
ms200788 <ms97179129@gmail.com>
2025-09-27 09:14:34 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -25,6 +25,7 @@ from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.utils.executor import start_webhook
+from aiogram.dispatcher.webhook import WebhookRequestHandler
 
 # -------------------------
 # Logging
